9eaaf566bd01085c3fba1b2abf28f949cfc8541e
Helsingbuss <info@helsingbuss.se>
2026-01-22 17:55:12 +0100

    Add SectionTitle section under hero

--- apps/site/app/page.tsx
+++ apps/site/app/page.tsx
@@ -1,10 +1,13 @@
 ﻿import HeroHeader from "../components/home/HeroHeader";
+import SectionTitle from "@/components/sections/SectionTitle";
 
 export default function HomePage() {
   return (
     <main style={{ width: "100%" }}>
       <HeroHeader />
-    </main>
+      <SectionTitle />
+</main>
   );
 }
+
 
