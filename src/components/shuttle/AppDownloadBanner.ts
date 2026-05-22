a8eca0e7c6d702aa2dd28582fb39b3e50b73b8e2
Helsingbuss <info@helsingbuss.se>
2026-05-22 16:27:03 +0200

    Add missing shuttle app download banner

---
+++ src/components/shuttle/AppDownloadBanner.ts
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function AppDownloadBanner() {
+  return React.createElement(
+    "div",
+    { className: "bg-[#194C66] px-4 py-2 text-center text-sm font-semibold text-white" },
+    "Helsingbuss Airport Shuttle – app och biljettfunktioner kommer snart."
+  );
+}
